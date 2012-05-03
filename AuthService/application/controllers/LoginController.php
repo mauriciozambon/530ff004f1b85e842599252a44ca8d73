d0d182f47a37139fe8c9b27b29e594f79eccfd5e
Jz <mauricio.jz.ic@gmail.com>
2012-05-03 19:48:59 -0300

    Adicionado Projeto Portal
    TODO:
    Criar singletons:
    Config
    Connection
    Log

--- AuthService/application/controllers/LoginController.php
+++ AuthService/application/controllers/LoginController.php
@@ -1,53 +1,51 @@
 <?php
 
-class LoginController extends Zend_Controller_Action
-{
-
-	public function init()
-	{
-		/* Initialize action controller here */
-		error_reporting(E_ALL);
-		ini_set('display_errors', '1');
+class LoginController extends Zend_Controller_Action {
+	
+	public function init() {
+		/*
+		 * Initialize action controller here
+		 */
+		error_reporting ( E_ALL );
+		ini_set ( 'display_errors', '1' );
 	}
-
-	public function indexAction()
-	{
+	
+	public function indexAction() {
 		// action body
 	}
-
-	public function authenticateAction()
-	{	
-		if ($this->_request->getMethod() == 'GET') {
-			$user = $this->_request->getParam('user');
-			$password = $this->_request->getParam('password');
-
+	
+	public function authenticateAction() {
+		if ($this->_request->getMethod () == 'GET') {
+			$user = $this->_request->getParam ( 'user' );
+			$password = $this->_request->getParam ( 'password' );
+			
 			/*
-			* response['error'] 			string
-			* response['authenticated']		boolean
-			*/
-		$response = array();
-		$response['error'] = null;
-
-		if (is_null($user) || is_null($password)) {
-			$response['error'] = "Missing user and/or password";
-		} else {
-			if ($this->verifyUserPassword($user, $password)) {
-				$response['authenticated'] = true;
+			 * response['error'] 			string 
+			 * response['authenticated']	boolean
+			 */
+			$response = array ();
+			$response ['error'] = null;
+			
+			if (is_null ( $user ) || is_null ( $password )) {
+				$response ['error'] = "Missing user and/or password";
 			} else {
-				$response['error'] = "Wrong user/password";
+				if ($this->verifyUserPassword ( $user, $password )) {
+					$response ['authenticated'] = true;
+				} else {
+					$response ['error'] = "Wrong user/password";
+				}
 			}
 		}
+		
+		$this->view->assign ( 'response', json_encode ( $response ) );
 	}
-
-	$this->view->assign('response', json_encode($response));
-}
-
-/*
-* Verify if user and password match.
-*/
-private function verifyUserPassword($user, $password) {
-	return true;
-}
+	
+	/*
+	 * Verify if user and password match.
+	 */
+	private function verifyUserPassword($user, $password) {
+		return true;
+	}
 
 }
 
