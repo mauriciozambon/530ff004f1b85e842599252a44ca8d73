05d374b6015697604ed4d84812f88aaf1c3d6d5b
Jz <mauricio.jz.ic@gmail.com>
2012-05-16 16:42:45 -0300

    Layout changes - Novos singletons

---
+++ Portal/library/Helpers/Config.php
@@ -0,0 +1,30 @@
+<?php
+
+class Helpers_Config {
+	
+	private static $instance;
+	const FILE = 'config.ini';
+	
+	private function __construct() {
+	}
+	
+	static private function getInstance() {
+		if (! isset ( self::$instance )) {
+			self::$instance = new self ();
+		}
+	}
+	
+	public static function get() {
+		self::getInstance();
+		
+		$filename = APPLICATION_PATH . '/configs/' . self::FILE;
+		if (file_exists ( $filename )) {
+			$config = new Zend_Config_Ini ( $filename );
+			
+			return $config;
+		}
+		return null;
+	}
+}
+
+?>
