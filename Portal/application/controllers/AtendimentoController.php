4663e31bfe998bc467178a66b31aa5becf937bbf
Matheus <m.magalhaes.ic@gmail.com>
2012-05-31 19:02:02 -0300

    Mudança nos parametros do cliente do serviço de atendimento. Método de abertura de chamado funcionando.

--- Portal/application/controllers/AtendimentoController.php
+++ Portal/application/controllers/AtendimentoController.php
@@ -7,16 +7,14 @@ class AtendimentoController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-        $authenticated = Helpers_Session::getInstance()->getSessVar("authenticated");
-        if (!$authenticated) {
-            $this->_helper->redirector('index', 'login');
-        }
-
-        //$id = Helpers_Connector::requestSoapService('atendimento', 'Abrir_Chamado', array('Abrir_Chamado' => array("Descricao" => "CHAMADO DE TESTE","IdCliente" => 00000000-0000-0000-0000-000000000007,"IdPedido"=> "10","IdProduto" => "23","IdSolicitante" => "44", "TipoChamado" => "1")));
-        //$id = Helpers_Connector::requestSoapService('atendimento', 'Abrir_Chamado', array('Abrir_Chamado' => array("IdCliente" => "00000000-0000-0000-0000-000000000007", "TipoChamado" => 1)));
-        $client = new Zend_Soap_Client("http://mc747atendimento.no-ip.org:2121/AtendimentoCliente.AtendimentoCliente.svc?wsdl",
-                        array('soap_version' => SOAP_1_1));
-        $id = $client->Abrir_Chamado(array('Abrir_Chamado' => array("Descricao" => "1", "IdCliente" => 00000000 - 0000 - 0000 - 0000 - 000000000007, "IdPedido" => "1", "IdProduto" => "1", "IdSolicitante" => "1", "TipoChamado" => 1)));
+               
+        $reqData = array( "Descricao" => "Pedido", 
+                          "IdCliente" => "00000000-0000-0000-0000-000000000007",
+                          "IdPedido" => "pedido_1",
+                          "IdProduto" => "produto_1",
+                          "IdSolicitante" => "solicitante_1",
+                          "TipoChamado" => "0");
+        $id = Helpers_Connector::requestSoapService('atendimento', 'Abrir_Chamado', array(array('chamado' => $reqData)));
         var_dump($id);
     }
 
--- Portal/application/controllers/BancoController.php
+++ Portal/application/controllers/BancoController.php
@@ -16,9 +16,10 @@ class BancoController extends Zend_Controller_Action
     public function depositoAction()
     {
         // action body
-        $result = Helpers_Connector::requestSoapService('banco', 'PagarViaDepositoBancario', array('PagarViaDepositoBancario' => array('agencia'=>123,'conta'=>123,'valor'=>10.50)));
-        //$client = new SoapClient("http://www.mc747.homologa.isat.com.br/BancoService.svc?wsdl");
-        //$result = $client->PagarViaDepositoBancario(array('agencia'=>1,'conta'=>1,'valor'=>1));
+        $params = array('agencia'=>1,
+                        'conta'=>12,
+                        'valor'=>123.1);
+        $result = Helpers_Connector::requestSoapService('banco2', 'PagarViaDepositoBancario', array('PagarViaDepositoBancario' => $params));
         var_dump($result);
     }
 
